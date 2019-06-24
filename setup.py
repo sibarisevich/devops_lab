c7591c2e4c6cf8cf756bc3c8389f55feea3ee119
sibarisevich <sergey.90@tut.by>
2019-06-24 11:47:13 +0300

    add config.ini in .whl

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-import setuptools
+#!/usr/bin/bash python
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name="monitor",
-    packages=setuptools.find_packages(),
+setup(
+    name='monitor',
+    packages=find_packages(),
     version="1.0",
     author="Siarhei Barysevich",
     author_email="siarhei_barysevich@epam.com",
     description="app for monitoring system info",
     license="MIT",
-    install_requires=['psutil==5.6.3']
-)
+    install_requires=['psutil==5.6.3'],
+    package_data={'monitor': ['config.ini'],},
+    include_package_data=True
+    )
