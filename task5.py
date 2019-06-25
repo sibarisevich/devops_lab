feb387288550ec9ffa3753b567ef01957e46e4f7
sibarisevich <sergey.90@tut.by>
2019-06-25 17:50:54 +0300

    test task5

---
+++ task5.py
@@ -0,0 +1,17 @@
+#! /bin/bash python
+def gorobotgo(robopath):
+	U = D = R = L = 0
+	U = robopath.count("u")
+	D = robopath.count("d")
+	R = robopath.count("r")
+	L = robopath.count("l")
+	if(U - D == 0) & (R - L == 0):
+#	    print("true")
+		return True
+	else:
+#	    print("folse")
+		return False
+
+if __name__ == "__main__":
+	robopath = input("Input robot path: ")
+	print(gorobotgo(robopath))
