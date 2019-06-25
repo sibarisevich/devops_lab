fbc78fdbbf870330859e13608e9b01d7bade1699
sibarisevich <sergey.90@tut.by>
2019-06-25 17:49:14 +0300

    изменил task5 для 5-го задания

--- task5.py
+++ task5.py
@@ -1,12 +1,15 @@
 #! /bin/bash python
-print("Input robot path")
-robopath = input()
-U = D = R = L = 0
-U = robopath.count("u")
-D = robopath.count("d")
-R = robopath.count("r")
-L = robopath.count("l")
-if(U - D == 0) & (R - L == 0):
-    print("true")
-else:
-    print("folse")
+def gorobotgo(robopath):
+    U = D = R = L = 0
+    U = robopath.count("u")
+    D = robopath.count("d")
+    R = robopath.count("r")
+    L = robopath.count("l")
+    if(U - D == 0) & (R - L == 0):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    robopath = input("Input robot path: ")
+    print(gorobotgo(robopath))
