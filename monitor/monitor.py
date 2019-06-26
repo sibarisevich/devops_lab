5e66cdcfe7ca2e379942ea355f35d105aaedb13e
sibarisevich <sergey.90@tut.by>
2019-06-26 22:43:06 +0300

    corrected sintaxis errors

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -4,20 +4,21 @@ import psutil
 from datetime import datetime
 import time
 import configparser
-import os
 import json
 
+
 class first:
-    """the first class in my life"""
+    """return information about natwork and mamory"""
     def network():
-        tmp=psutil.net_io_counters().bytes_sent
+        tmp = psutil.net_io_counters().bytes_sent
         return (tmp)
     def vmemory():
-        tmp=psutil.virtual_memory().total
+        tmp = psutil.virtual_memory().total
         return (tmp)
     def swap():
-        tmp=psutil.swap_memory().used
+        tmp = psutil.swap_memory().used
         return (tmp)
+
 
 i = n = 0
 
@@ -31,9 +32,6 @@ if count != 0:
     i += 1
 
 report = open("systemstatus." + reportformat, "w")
-
-if reportformat == "txt":
-    report.write("Snapshot number; Carrent Time; % CPU load; Memory total; Swap memory; Disk IO; Networks bites send \n")
 
 while i <= count:
     n += 1
@@ -51,19 +49,18 @@ while i <= count:
             str(first.network()) + "\n"
         report.write(temp)
         print(temp)
-        time.sleep(timetosleep*60)
+        time.sleep(timetosleep * 60)
 
     if reportformat == "json":
         temp = json.dumps({
             "Snapshot number": str(n),
             "Carrent Time": str(datetime.now().strftime('%d.%m %H:%M:%S')),
             "% CPU load": str(psutil.cpu_percent(interval=1)),
-             "Memory total": str(first.vmemory()),
+            "Memory total": str(first.vmemory()),
             "Swap memory": str(first.swap()),
             "Disk IO": str(psutil.disk_io_counters().read_count),
-            "Networks bites send": str(first.network())
-            }, indent=4)
+            "Networks bites send": str(first.network())}, indent=4)
         report.write(temp)
-        time.sleep(timetosleep*60)
+        time.sleep(timetosleep * 60)
 
 report.close()
