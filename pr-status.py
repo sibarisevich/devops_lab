4989e43d98b224ee62ee0900337da7af66ecf32d
sibarisevich <sergey.90@tut.by>
2019-06-30 12:49:07 +0300

    chenged code based on comment

--- pr-status.py
+++ pr-status.py
@@ -7,12 +7,27 @@ arg = argparse.ArgumentParser()
 arg.add_argument("--user", help="insert github username")
 arg.add_argument("--repo", help="insert gthub repository")
 arg.add_argument("--pull", help="insert number of github pull-request")
-arg.add_argument("--version", help="output version the app")
+arg.add_argument("--login", help="insert github login")
+arg.add_argument("--token", help="insert github token")
+arg.add_argument("--version", action='version', version='%(prog)s 1.0')
+
+arg.add_argument('--all', help="output all available parameters", action="store_true")
+arg.add_argument('-du', help='output date of updated pull request', action="store_true")
+arg.add_argument('-dc', help='output day of created pull request', action="store_true")
+arg.add_argument('-il', help='output number of inserted line(s)', action="store_true")
+arg.add_argument('-dl', help='output number of delete line(s)', action="store_true")
+arg.add_argument('-cr', help='output creator pull request', action="store_true")
+arg.add_argument('-dfc', help='output number of day after created pull request', action="store_true")
 
 arg = arg.parse_args()
 
-token = 'ba1dd0090b23c0b96d161a424fc8da24caa058d5'
-login = 'sibarisevich'
+token = login = ''
+
+if arg.login != None:
+    login = arg.login
+
+if arg.token != None:
+    token = arg.token
 
 if arg.user is None:
     accaunt = 'alenaPy'
@@ -29,9 +44,6 @@ if arg.pull is None:
 else:
     numberpull = arg.pull
 
-if arg.version:
-    print("version 1.0")
-    quit()
 
 link = "https://api.github.com/repos/" + accaunt + "/" + repo + "/pulls/" + numberpull
 r = requests.get(link, auth=(login, token))
@@ -45,9 +57,15 @@ deliteline = m['deletions']
 d1 = datetime.datetime.strptime(create, "%Y-%m-%dT%H:%M:%SZ")
 daysopened = datetime.datetime.now() - d1
 
-print(" data obnovleniya: ", update)
-print("den sozdaniya: ", create[8:10])
-print("dobavleno strok: ", addline)
-print("udaleno strok: ", deliteline)
-print("sozdatel: ", usercreated)
-print("dney s sozdaniya: ", daysopened.days)
+if arg.all or arg.du:
+    print("date of updated ", update)
+if arg.all or arg.dc:
+    print("day of created: ", create[8:10])
+if arg.all or arg.il:
+    print("inserted line(s): ", addline)
+if arg.all or arg.dl:
+    print("deleted line(s): ", deliteline)
+if arg.all or arg.cr:
+    print("creator: ", usercreated)
+if arg.all or arg.dfc:
+    print("number of day after created: ", daysopened.days)
